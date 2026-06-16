7a0b4612b2855dbb881aa9e7f93cdec49e06ebb6
Abinesh-CybSec <abineshvas@gmail.com>
2026-06-16 12:32:28 +0530

    fix: exclude setup.ts from test discovery, only match *.test.* files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|lucide-react-native)',
   ],
